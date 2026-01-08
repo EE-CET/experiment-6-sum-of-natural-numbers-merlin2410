2b02837464f08c6dfc68d6cb44b3c63336ec70cb
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2026-01-08 22:18:17 +0530

    corrected the errors

--- src/SumOfNaturalNumbers.java
+++ src/SumOfNaturalNumbers.java
@@ -4,7 +4,7 @@ public class SumOfNaturalNumbers {
 		return n*(n+1)/2;
 	}
 
-	public static void main(String[] args){]
+	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println(n);
