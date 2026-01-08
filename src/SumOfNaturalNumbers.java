e9fb9ef30145afc46fb00789d7aee4358d3bdd26
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2026-01-08 22:22:30 +0530

    corrected errors

--- src/SumOfNaturalNumbers.java
+++ src/SumOfNaturalNumbers.java
@@ -1,13 +1,14 @@
+import java.util.Scanner;
 public class SumOfNaturalNumbers {
 
-	public int sum(int n){
+	public static int sum(int n){
 		return n*(n+1)/2;
 	}
 
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println(n);
+		System.out.println(sum(n));
 	}
     
 }
